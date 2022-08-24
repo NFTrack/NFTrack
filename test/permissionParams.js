6e466e52728878482d1e7869205c05e30403cbf7
NFTrack <105763451+NFTrack@users.noreply.github.com>
2022-08-24 11:47:09 +0800

    Create permissionParams.js

---
+++ test/permissionParams.js
@@ -0,0 +1,13 @@
+const { bn } = require('@aragon/contract-helpers-test')
+
+const permissionParamEqOracle = (oracleAddress) => {
+  // Set role such that the Oracle canPerform() function is used to determine the permission
+  const argId = '0xCB' // arg 203 - Oracle ID
+  const op = '01'      // equal
+  const value = oracleAddress.slice(2).padStart(60, 0) // 60 as params are uint240
+  return bn(`${argId}${op}${value}`)
+}
+
+module.exports = {
+  permissionParamEqOracle
+}
